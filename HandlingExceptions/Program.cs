a803f86736c890e42ca065eff3b34a1cda198077
manel talbi <talbimanel20@gmail.com>
2022-01-13 11:17:38 +0100

    using try..catch

--- HandlingExceptions/Program.cs
+++ HandlingExceptions/Program.cs
@@ -4,9 +4,36 @@
     {
         static void Main(string[] args)
         {
-            string content = File.ReadAllText(@"c:/lesson 17/WriteText.txt");
-            Console.WriteLine(content);
+            try
+            {
+                string content = File.ReadAllText(@"c:/lesson 17/WriteTex.txt");
+                Console.WriteLine(content);
+               
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("there was a problem!");
+                Console.WriteLine("Make sure the name of the file is named correctly WriteTexte.txt ");
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("there was a problem!");
+                Console.WriteLine(@"Make sure the directory c:/lesson 17 exists ");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("there was a problem!");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                //code to finalize
+                //setting objects to null
+                // Closing database connections
+                Console.WriteLine("Closing application now...");
+            }
             Console.ReadLine();
+
         }
     }
 }
