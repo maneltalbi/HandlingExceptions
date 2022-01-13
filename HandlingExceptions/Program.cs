77d213fd1378d543f6998c9ad12183b93f76b0db
manel talbi <talbimanel20@gmail.com>
2022-01-13 10:22:39 +0100

    initial commit

---
+++ HandlingExceptions/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace EnumsAndSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string content = File.ReadAllText(@"c:/lesson 17/WriteText.txt");
+            Console.WriteLine(content);
+            Console.ReadLine();
+        }
+    }
+}
